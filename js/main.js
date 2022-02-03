624b7301932fcff20fa50105df49d7fc7fed86c1
Alexis Morin <the.alexis.morin@gmail.com>
2022-02-03 14:51:22 -0500

    scrollIntoView

--- js/main.js
+++ js/main.js
@@ -1,56 +1,59 @@
 var headerMargin = 0;
 
-function sizeBg(){
+function sizeBg() {
   var bg = document.getElementsByClassName("body-img")[0];
-  var introHeight = window.getComputedStyle(document.getElementById("intro")).height;
-  introHeight = parseInt(introHeight.substring(0, introHeight.length-2));
+  var introHeight = window.getComputedStyle(
+    document.getElementById("intro")
+  ).height;
+  introHeight = parseInt(introHeight.substring(0, introHeight.length - 2));
   var header = document.getElementsByTagName("header")[0];
   var headerHeight = window.getComputedStyle(header).height;
-  headerHeight = parseInt(headerHeight.substring(0, headerHeight.length-2));
+  headerHeight = parseInt(headerHeight.substring(0, headerHeight.length - 2));
 
-  var headerMargin = window.getComputedStyle(header).marginTop;//for tablets
-  headerMargin = parseInt(headerMargin.substring(0, headerMargin.length-2));
+  var headerMargin = window.getComputedStyle(header).marginTop; //for tablets
+  headerMargin = parseInt(headerMargin.substring(0, headerMargin.length - 2));
 
-  var totalHeight = introHeight+ headerHeight + headerMargin;
-  bg.style.height = totalHeight+"px";
+  var totalHeight = introHeight + headerHeight + headerMargin;
+  bg.style.height = totalHeight + "px";
 }
 
-function resizeIntroDiv(){
+function resizeIntroDiv() {
   intro = document.getElementById("intro");
-  if(intro != null){
-    header = document.getElementsByTagName("header")[0]
-    headerMargin = window.getComputedStyle(header).marginTop
-    headerMargin = headerMargin.substring(0, headerMargin.length-2)
-    headerHeight = window.getComputedStyle(header).height
-    headerHeight = headerHeight.substring(0, headerHeight.length-2)
+  if (intro != null) {
+    header = document.getElementsByTagName("header")[0];
+    headerMargin = window.getComputedStyle(header).marginTop;
+    headerMargin = headerMargin.substring(0, headerMargin.length - 2);
+    headerHeight = window.getComputedStyle(header).height;
+    headerHeight = headerHeight.substring(0, headerHeight.length - 2);
 
-    setIntroDivHeight(intro, window.innerHeight-headerMargin - headerHeight);
+    setIntroDivHeight(intro, window.innerHeight - headerMargin - headerHeight);
   }
 }
 
-var vidRatio = 16/9;
-function resizeBgVideo(){
+var vidRatio = 16 / 9;
+function resizeBgVideo() {
   var vid = document.getElementById("share-video");
-  if(vid != null){
+  if (vid != null) {
     var pageHeight = window.innerHeight;
     var pageWidth = window.innerWidth;
-    if((pageWidth/pageHeight) > vidRatio){//ratio is wider than 16:9
+    if (pageWidth / pageHeight > vidRatio) {
+      //ratio is wider than 16:9
       vid.style.width = "100%";
       vid.style.height = "auto";
       vid.style.marginLeft = "0";
-    }else{
+    } else {
       var widthOfVideoAtHeight = pageHeight * vidRatio;
       var videoOverflow = widthOfVideoAtHeight - pageWidth;
-      var newMargin = -(videoOverflow/2);
+      var newMargin = -(videoOverflow / 2);
       vid.style.width = "auto";
       vid.style.height = "100%";
-      vid.style.marginLeft = -(videoOverflow/2)+"px";
+      vid.style.marginLeft = -(videoOverflow / 2) + "px";
     }
   }
 }
 
-function setIntroDivHeight(div, height){
-  div.style.height = height+"px";
+function setIntroDivHeight(div, height) {
+  div.style.height = height + "px";
 }
 
 greetings = [
@@ -62,46 +65,9 @@ greetings = [
   "Hallå där",
   "Bonjour",
   "Hola",
-  "Hallo"
-]
+  "Hallo",
+];
 
-function changeGreeting(){
+function changeGreeting() {
   //for a later time
 }
-
-window.bringIntoView_started = 0;
-
-window.bringIntoView_ends = 0;
-
-window.bringIntoView_y = 0;
-
-window.nonDeprecScrTop = document.scrollingElement ? document.scrollingElement : document.body;
-//window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
-
-window.bringIntoView_tick = function() {
-    var distanceLeft, dt, duration, t, travel;
-    t = Date.now();
-    if (t < window.bringIntoView_ends) {
-      dt = t - window.bringIntoView_started;
-      duration = window.bringIntoView_ends - window.bringIntoView_started;
-      distanceLeft = window.bringIntoView_y - window.nonDeprecScrTop.scrollTop;
-      if (Math.abs(distanceLeft) < 1) {
-        return window.nonDeprecScrTop.scrollTop = window.bringIntoView_y;
-      } else {
-        travel = distanceLeft * (dt / duration);
-        //console.log('%f => %f (%f) dt=%f / %f (%f)', window.nonDeprecScrTop.scrollTop, window.bringIntoView_y, travel, dt, duration, dt / duration);
-        window.nonDeprecScrTop.scrollTop += travel;
-        return window.requestAnimationFrame(window.bringIntoView_tick);
-      }
-    } else {
-      return window.nonDeprecScrTop.scrollTop = window.bringIntoView_y;
-    }
-  };
-
-window.bringIntoView = function(e, duration) {
-    window.bringIntoView_started = Date.now();
-    window.bringIntoView_ends = window.bringIntoView_started + duration;
-    window.bringIntoView_y = Math.min(window.nonDeprecScrTop.scrollTop + e.getBoundingClientRect().top, document.body.scrollHeight - window.innerHeight);
-    //console.log(window.nonDeprecScrTop.scrollTop + e.getBoundingClientRect().top, document.body.scrollHeight, window.bringIntoView_y);
-    return window.requestAnimationFrame(window.bringIntoView_tick);
-  };
