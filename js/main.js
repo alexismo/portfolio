3cc52fb530a66dbb7798a7ecdf18f5605c8b553d
Alexis Morin <the.alexis.morin@gmail.com>
2025-03-02 19:39:30 -0500

    Compass-case-study (#7)
    * first commit
    
    * compass progress
    
    * bulk actions and listing cards

--- js/main.js
+++ js/main.js
@@ -71,3 +71,72 @@ greetings = [
 function changeGreeting() {
   //for a later time
 }
+
+const pictures = document.querySelectorAll('[light-box]');
+const state = { open: false };
+let modalPic = null;
+let placeholder = null;
+pictures.forEach(picture => {
+  picture.addEventListener('click', event => {
+    if (!state.open) {
+      lightboxImage(picture);
+    }
+  })
+})
+
+
+function lightboxImage(picture) {
+  modalPic = picture.querySelector('img');
+  modalPic.style.viewTransitionName = '--light-box-img';
+
+  function mutate() {
+    lightbox.innerHTML = picture.querySelector(':scope img').outerHTML;
+
+    placeholder = placeholdImage(modalPic);
+    modalPic.parentElement.appendChild(placeholder);
+    modalPic.style.display = 'none';
+
+    lightbox.showModal();
+  }
+  document.startViewTransition(mutate);
+}
+
+async function hideLightbox() {
+  function mutate() {
+    lightbox.close();
+    modalPic.parentElement.removeChild(placeholder);
+    modalPic.style.display = null;
+
+    modalPic.style.viewTransitionName = null;
+    lightbox.innerHTML = '';
+  }
+
+  await document.startViewTransition(mutate).updateCallbackDone;
+
+
+  modalPic = null;
+  placeholder = null;
+}
+
+function placeholdImage(img) {
+  let placeholder = document.createElement('div')
+  placeholder.style.width = img.clientWidth + 'px'
+  placeholder.style.height = img.clientHeight + 'px'
+  return placeholder
+}
+
+lightbox.addEventListener('beforetoggle', event => {
+  if (event.newState === 'closed') {
+    hideLightbox();
+  }
+  state.open = event.newState === 'open';
+});
+
+lightbox.addEventListener('click', function (event) {
+  var rect = lightbox.getBoundingClientRect();
+  var isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+    rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+  if (!isInDialog) {
+    hideLightbox();
+  }
+});
